f67a6a9319199df2ae596b1b6f39b6f3154d090c
hightmike <baobab-borisov@mail.ru>
2020-07-08 14:54:32 +0300

    add ArrayLeftRotation

---
+++ src/Preparation/Arrays/ArrayLeftRotation.java
@@ -0,0 +1,64 @@
+package Preparation.Arrays;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ArrayLeftRotation {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+
+        int[] cur = new int[a.length];
+        System.arraycopy(a, 0, cur, 0, a.length);
+        for (int j = 0; j < d; j++) {
+            for (int i = a.length-1; i !=0; i--) {
+                a[i-1] = cur[i];
+            }
+            a[a.length-1] = cur[0];
+            System.arraycopy(a, 0, cur, 0, a.length);
+        }
+        return a;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i : result) {
+            System.out.println(i);
+        }
+
+//        for (int i = 0; i < result.length; i++) {
+//            bufferedWriter.write(String.valueOf(result[i]));
+//
+//            if (i != result.length - 1) {
+//                bufferedWriter.write(" ");
+//            }
+//        }
+//
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
